fef7cf9d0861ee4bf7040568ab9f89fc300ad9e3
jaderson.brandao <jaderson.brandao@gmail.com>
2022-11-28 01:03:16 -0300

    Obtentdo o tempo da partida

--- src/main/java/br/com/brasileirao/api/util/ScrapingUtil.java
+++ src/main/java/br/com/brasileirao/api/util/ScrapingUtil.java
@@ -15,7 +15,7 @@ public class ScrapingUtil {
     private static final String COMPLEMENTO_URL_GOOGLE=  "&hl=pt-BR";
 
     public static void main(String[] args) {
-        String url = BASE_URL_GOOGLE+ "palmeiras+x+corinthians+08/08/2022" + COMPLEMENTO_URL_GOOGLE;
+        String url = BASE_URL_GOOGLE+ "Bélgica+x+Marrocos" + COMPLEMENTO_URL_GOOGLE;
 
         ScrapingUtil scraping = new ScrapingUtil();
         scraping.obtemInformacoesPartida(url);
@@ -28,10 +28,42 @@ public class ScrapingUtil {
             document = Jsoup.connect(url).get();
             String title = document.title();
             LOGGER.info("Título da pagina: {}", title);
+
+            StatusPartida statusPartida = obtemStatusPartida(document);
+            LOGGER.info("Status partida: {}", statusPartida);
+
+            String tempoPartida = obtemTempoPartida(document);
+            LOGGER.info("Tempo partida: {}", tempoPartida);
         } catch (IOException e) {
             LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE COM JSOUP -> {}", e.getMessage());
             e.printStackTrace();
         }
         return partida;
+    }
+
+    public StatusPartida obtemStatusPartida(Document document) {
+        StatusPartida statusPartida = StatusPartida.PARTIDA_NAO_INICIADA;
+        boolean isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+        if (!isTempoPartida) {
+            String tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first().text();
+            statusPartida = StatusPartida.PARTIDA_ENCERRADA;
+        }
+        return statusPartida;
+    }
+    public String obtemTempoPartida(Document document) {
+        String tempoPartida = null;
+        boolean isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
+        if (!isTempoPartida) {
+            tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first().text();
+        }
+        return corrigeTempoPartida(tempoPartida);
+    }
+
+    public String corrigeTempoPartida(String tempo) {
+        if (tempo.contains("'")) {
+            return tempo.replace(" ","").replace("'", " min");
+        }  else {
+            return tempo;
+        }
     }
 }
